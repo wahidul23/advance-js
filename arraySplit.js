95c3be32b622966fc1d96da0c309a590772b569d
Wahidul23 <wahidcse69@gmail.com>
2021-02-03 23:08:39 +0600

    split, slice

---
+++ arraySplit.js
@@ -0,0 +1,7 @@
+const num = [1, 2, 3, 4, 5,6];
+// const part = num.slice(2, 4);
+const removed = num.splice(2,4, 56, 44, 53, 77);
+// console.log(removed);
+// console.log(num);
+ const join = num.join(" wahidul ");
+ console.log(join);
---
+++ scope.js
@@ -0,0 +1,4 @@
+function sum(num1, num2){
+    let result = num1 + num2;
+    return result;
+}
