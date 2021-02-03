1c6f000a37e0e9cb77f29ecd97e40c4714c1ed88
Wahidul23 <wahidcse69@gmail.com>
2021-02-03 16:10:36 +0600

    null & undefined clear for interview

---
+++ null-Vs-Undefined.js
@@ -0,0 +1,17 @@
+//Undefined values:
+//let study;
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return;
+// }
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+const premik = {name:'kalam', phone:83939};
+console.log(premik.gf);
+let fun = undefined;
+console.log(fun);
+
+let ages = [12, 32,43]
+console.log(ages[10]);
