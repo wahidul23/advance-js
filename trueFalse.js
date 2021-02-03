86cecf6da1399fe186a4d421cd882894dbb4eff6
Wahidul23 <wahidcse69@gmail.com>
2021-02-03 15:32:37 +0600

    true vs false

---
+++ trueFalse.js
@@ -0,0 +1,16 @@
+//falsy value:
+//0
+//null
+//undefine
+//NanN
+//""
+//false
+//truthy value:
+//" " "0"
+const age = 4;
+if(age > 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
